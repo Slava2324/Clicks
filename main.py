335360e630b51f11b54204e784a1eb774cf7f9d4
Slava2324 <mirvec2010@gmail.com>
2024-12-11 19:31:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from requests import *
+import requests as req
 
 
 with open("api.txt", "r") as api_key:
     API_KEY = api_key.read()
 
-print(API_KEY)
+params = {"access_token": API_KEY,
+          "v": "5.199"}
+
+response = req.get(url="https://api.vk.ru/method/utils.getServerTime", params=params)
+response.raise_for_status()
+print(response.ok)
+print(response.text)
 
